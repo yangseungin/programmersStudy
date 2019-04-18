acc08e1bd36a95a1995022165e0ec14cc8c82509
yangseungin <rhfpdk92@naver.com>
2019-04-18 23:29:56 +0900

    거스름돈시작

---
+++ progreamers/src/lv3/lv3_change.java
@@ -0,0 +1,27 @@
+package lv3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class lv3_change {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		//int n=Integer.parseInt(br.readLine());
+		int n=5;
+		int[] money = {1,2,5};
+		
+		
+	
+	}
+	public int solution(int n,int[] money){
+		int answer=0;
+		
+		return answer;
+	}
+
+}
