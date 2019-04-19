30d098315d97c5e6778a0250637c483f91865f6f
yangseungin <rhfpdk92@naver.com>
2019-04-19 23:44:44 +0900

    거스름돈 dp

--- progreamers/src/lv3/lv3_change.java
+++ progreamers/src/lv3/lv3_change.java
@@ -11,6 +11,17 @@ public class lv3_change {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		//슬러줘야할 금액, 동전이 들어간 배열을 줄 거슬러줄수있는 방법의 개수리턴
+		
+		
+		/*		0	1	2	3	4	5
+		 * 1	0	1	2	3	4	5
+		 * 2	0	0	1		2
+		 * 5	0					1
+		
+		
+*/		
+		
 		//int n=Integer.parseInt(br.readLine());
 		int n=5;
 		int[] money = {1,2,5};
