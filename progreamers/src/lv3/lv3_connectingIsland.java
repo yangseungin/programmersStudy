2f5f593c8ec1893328b4d2af2df406719a0702fb
yangseungin <rhfpdk92@naver.com>
2019-04-22 23:36:06 +0900

    섬연결하기 생각

---
+++ progreamers/src/lv3/lv3_connectingIsland.java
@@ -0,0 +1,16 @@
+package lv3;
+
+public class lv3_connectingIsland {
+	public static void main(String[] args) {
+		
+//		음 이문제는 연결정보 넣고 시작점에서 짧은거리 이동하면서 값더하면 될것같
+		
+	}
+	public static class Solution {
+	    public int solution(int n, int[][] costs) {
+	        int answer = 0;
+	        return answer;
+	    }
+	}
+
+}
