1c6f47d7a5d6e57385eeed8640583ac81a50e324
yangseungin <rhfpdk92@naver.com>
2019-04-23 09:09:58 +0900

    섬연결하기 mst로 풀어보자

--- progreamers/src/lv3/lv3_connectingIsland.java
+++ progreamers/src/lv3/lv3_connectingIsland.java
@@ -4,6 +4,7 @@ public class lv3_connectingIsland {
 	public static void main(String[] args) {
 		
 //		음 이문제는 연결정보 넣고 시작점에서 짧은거리 이동하면서 값더하면 될것같
+		// 그리디라 적혀있으니 최소신장트리로 프림이나 크루스칼 알고리즘으로 풀어보자
 		
 	}
 	public static class Solution {
